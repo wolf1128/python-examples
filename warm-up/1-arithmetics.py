76f6fdb3d1f065fc85be799e323b0d868f1fab74
Salar Farahmand <professor.dev.run@gmail.com>
2024-11-24 00:58:11 +0100

    Added 1-arithmetic-ops

---
+++ warm-up/1-arithmetics.py
@@ -0,0 +1,21 @@
+# Basic Arithmetic operations
+
+def arithmetic_operations(a, b):
+    # Addition
+    print("Addition: ", a + b)
+    # Subtraction
+    print("Subtraction: ", a - b)
+    # Multiplication
+    print("Multiplication: ", a * b)
+    # Division
+    print("Division: ", a / b)
+    # Modulus
+    print("Modulus: ", a % b)
+    # Exponentiation
+    print("Exponentiation: ", a ** b)
+
+
+if __name__ == "__main__":
+    a = 10
+    b = 20
+    arithmetic_operations(a, b)
---
+++ warm-up/README.md
@@ -0,0 +1,5 @@
+# Tips
+
+0. The last line of code if **name** == "**main**": ensures that the main() function is called only when the script is run directly, not when it is imported as a module in another script.
+
+1.
